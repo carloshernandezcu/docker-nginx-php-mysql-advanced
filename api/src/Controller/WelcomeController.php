768f97019e03a794b811d1c80986163af8c32606
Carlos Hernandez <carlos.hernandez@giffits.de>
2018-12-03 04:02:57 +0000

    Rename controller

--- api/src/Controller/WelcomeController.php
+++ api/src/Controller/WelcomeController.php
@@ -6,7 +6,7 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class LuckyController extends AbstractController
+class WelcomeController extends AbstractController
 {
     /**
      * @Route("/api/welcome")
